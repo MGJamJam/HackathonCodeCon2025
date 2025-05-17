58327657cb733f2e73c0621eef0c912db087913c
monica hillman <mmhillman1@ucs.br>
2025-05-17 15:01:56 -0300

    feat: face error componente

---
+++ src/components/face-error/face-error.jsx
@@ -0,0 +1,9 @@
+export function FaceError() {
+  return (
+    <div className="absolute inset-0 flex items-center justify-center">
+      <p className="text-red-500 text-center bg-black bg-opacity-60 px-4 py-2 rounded">
+        Traição botânica detectada!
+      </p>
+    </div>
+  );
+}
