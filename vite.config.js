9ee585efb7141b11a673c7653fa777e59af01274
dianasaramago <dianasaramagog@gmail.com>
2025-05-17 13:33:05 -0300

    refactor

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
@@ -10,12 +9,3 @@ export default defineConfig({
     tailwindcss()
   ],
 })
-=======
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-});
->>>>>>> main
